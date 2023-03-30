99fc6347bc40841f9ab3fb5e5885e62e9786de1e
Naykat <65075625+Naykat@users.noreply.github.com>
2023-03-30 19:06:49 +0300

    Update BinaryTree.py

--- Trees/BinaryTree.py
+++ Trees/BinaryTree.py
@@ -14,7 +14,7 @@ class BinaryTree:
         else:
             self._insert(data, self.root)
 
-    def _insert(self, data, node):
+    def _insert(self, data, node: Node):
         if data < node.data:
             if node.left is None:
                 node.left = Node(data)
@@ -32,7 +32,7 @@ class BinaryTree:
         else:
             return self._search(data, self.root)
 
-    def _search(self, data, node):
+    def _search(self, data, node: Node):
         if node is None:
             return False
         elif data == node.data:
@@ -41,3 +41,39 @@ class BinaryTree:
             return self._search(data, node.left)
         else:
             return self._search(data, node.right)
+
+    def delete(self, data):
+        if self.root is None:
+            return
+
+        self.root = self._delete(data, self.root)
+
+    def _delete(self, data, node: Node):
+        if node is None:
+            return node
+
+        if data < node.data:
+            node.left = self._delete(data, node.left)
+        elif data > node.data:
+            node.right = self._delete(data, node.right)
+        else:
+            if node.left is None:
+                temp = node.right
+                node = None
+                return temp
+            elif node.right is None:
+                temp = node.left
+                node = None
+                return temp
+
+            temp = self._find_min_node(node.right)
+            node.data = temp.data
+            node.right = self._delete(temp.data, node.right)
+
+        return node
+
+    def _find_min_node(self, node: Node):
+        current = node
+        while current.left is not None:
+            current = current.left
+        return current
