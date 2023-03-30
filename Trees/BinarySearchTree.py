119811093dbb9ef82e8e76cc5efe0b8360c28faa
Naykat <65075625+Naykat@users.noreply.github.com>
2023-03-30 19:07:40 +0300

    Update BinarySearchTree.py

--- Trees/BinarySearchTree.py
+++ Trees/BinarySearchTree.py
@@ -1,5 +1,5 @@
 class Node:
-    def __init__(self, value: int):
+    def __init__(self, value):
         self.value = value
         self.left = None
         self.right = None
@@ -8,14 +8,14 @@ class BinarySearchTree:
     def __init__(self):
         self.root = None
 
-    def insert(self, value: int):
+    def insert(self, value):
         new_node = Node(value)
         if self.root is None:
             self.root = new_node
         else:
             self._insert(value, self.root)
 
-    def _insert(self, value: int, current_node: Node):
+    def _insert(self, value, current_node: Node):
         if value < current_node.value:
             if current_node.left is None:
                 current_node.left = Node(value)
@@ -29,13 +29,13 @@ class BinarySearchTree:
         else:
             print("Value already exists in the tree.")
 
-    def search(self, value: int):
+    def search(self, value):
         if self.root is None:
             return
         else:
             return self._search(value, self.root)
 
-    def _search(self, value: int, current_node: Node):
+    def _search(self, value, current_node: Node):
         if current_node is None:
             return
         elif current_node.value == value:
@@ -45,13 +45,13 @@ class BinarySearchTree:
         else:
             return self._search(value, current_node.right)
 
-    def delete(self, value: int):
+    def delete(self, value):
         if self.root is None:
             return
 
         self.root = self._delete(value, self.root)
 
-    def _delete(self, value: int, current_node: Node):
+    def _delete(self, value, current_node: Node):
         if current_node is None:
             return current_node
 
