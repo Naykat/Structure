b6e4598b5a702093b08cd5a1aecb450d6feeba79
Naykat <65075625+Naykat@users.noreply.github.com>
2023-03-30 18:54:49 +0300

    BST Fix

--- Trees/BinarySearchTree.py
+++ Trees/BinarySearchTree.py
@@ -1,8 +1,8 @@
 class Node:
     def __init__(self, value):
         self.value = value
-        self.left_child = None
-        self.right_child = None
+        self.left = None
+        self.right = None
 
 class BinarySearchTree:
     def __init__(self):
@@ -17,15 +17,15 @@ class BinarySearchTree:
 
     def _insert(self, value, current_node):
         if value < current_node.value:
-            if current_node.left_child is None:
-                current_node.left_child = Node(value)
+            if current_node.left is None:
+                current_node.left = Node(value)
             else:
-                self._insert(value, current_node.left_child)
+                self._insert(value, current_node.left)
         elif value > current_node.value:
-            if current_node.right_child is None:
-                current_node.right_child = Node(value)
+            if current_node.right is None:
+                current_node.right = Node(value)
             else:
-                self._insert(value, current_node.right_child)
+                self._insert(value, current_node.right)
         else:
             print("Value already exists in the tree.")
 
@@ -41,6 +41,38 @@ class BinarySearchTree:
         elif current_node.value == value:
             return True
         elif value < current_node.value:
-            return self._search(value, current_node.left_child)
+            return self._search(value, current_node.left)
         else:
-            return self._search(value, current_node.right_child)
+            return self._search(value, current_node.right)
+
+    def delete(self, value):
+        if self.root is None:
+            return
+
+        self.root = self._delete(value, self.root)
+
+    def _delete(self, value, current_node):
+        if current_node is None:
+            return current_node
+
+        if value < current_node.value:
+            current_node.left = self._delete(value, current_node.left)
+        elif value > current_node.value:
+            current_node.right = self._delete(value, current_node.right)
+        else:
+            if current_node.left is None:
+                return current_node.right
+            elif current_node.right is None:
+                return current_node.left
+            else:
+                temp = self._find_min_node(current_node.right)
+                current_node.value = temp.value
+                current_node.right = self._delete(temp.value, current_node.right)
+
+        return current_node
+
+    def _find_min_node(self, node):
+        current = node
+        while current.left is not None:
+            current = current.left
+        return current
