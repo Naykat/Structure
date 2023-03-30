94bb7cb248eb3a6730e0862b5cde3ce5ce003bf9
Naykat <65075625+Naykat@users.noreply.github.com>
2023-03-31 00:04:12 +0300

    Global rework

--- Heap.py
+++ Heap.py
@@ -1,54 +1,126 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+        self.parent = None
+
 class Heap:
     def __init__(self):
-        self.heap = []
+        self.root = None
 
-    def parent(self, i):
-        return (i - 1) // 2
-
-    def left(self, i):
-        return 2 * i + 1
-
-    def right(self, i):
-        return 2 * i + 2
-
-    def get_max(self):
-        if self.heap:
-            return self.heap[0]
+    def insert(self, value):
+        node = Node(value)
+        if self.root is None:
+            self.root = node
         else:
+            queue = [self.root]
+            while queue:
+                curr = queue.pop(0)
+                if curr.left is None:
+                    curr.left = node
+                    node.parent = curr
+                    break
+                elif curr.right is None:
+                    curr.right = node
+                    node.parent = curr
+                    break
+                else:
+                    queue.append(curr.left)
+                    queue.append(curr.right)
+            self.heapify_up(node)
+
+    def extract_min(self):
+        if self.root is None:
             return None
+        elif self.root.left is None and self.root.right is None:
+            min_value = self.root.value
+            self.root = None
+            return min_value
+        else:
+            queue = [self.root]
+            min_node = self.root
+            while queue:
+                curr = queue.pop(0)
+                if curr.left is not None:
+                    if curr.left.value < min_node.value:
+                        min_node = curr.left
+                    queue.append(curr.left)
+                if curr.right is not None:
+                    if curr.right.value < min_node.value:
+                        min_node = curr.right
+                    queue.append(curr.right)
+            min_value = min_node.value
+            self.delete(min_node)
+            return min_value
+        
+    def delete(self, node: Node) -> None:
+        if self.root is None:
+            return None
+        elif self.root.left is None and self.root.right is None:
+            self.root = None
+        else:
+            queue = [self.root]
+            while queue:
+                curr = queue.pop(0)
+                if curr is node:
+                    last_node = self.get_last_node()
+                    curr.value = last_node.value
+                    self.delete_last_node(last_node)
+                    break
+                if curr.left is not None:
+                    queue.append(curr.left)
+                if curr.right is not None:
+                    queue.append(curr.right)
+            self.heapify_down(curr)
 
-    def insert(self, item):
-        self.heap.append(item)
-        self._percolate_up(len(self.heap) - 1)
+    def get_last_node(self) -> Node:
+        if self.root is None:
+            return None
+        elif self.root.left is None and self.root.right is None:
+            return self.root
+        else:
+            queue = [self.root]
+            last_node = self.root
+            while queue:
+                curr = queue.pop(0)
+                if curr.left is not None:
+                    last_node = curr.left
+                    queue.append(curr.left)
+                if curr.right is not None:
+                    last_node = curr.right
+                    queue.append(curr.right)
+            return last_node
 
-    def _percolate_up(self, i):
-        while i > 0:
-            parent = self.parent(i)
-            if self.heap[i] > self.heap[parent]:
-                self.heap[i], self.heap[parent] = self.heap[parent], self.heap[i]
-                i = parent
+    def delete_last_node(self, node: Node) -> None:
+        if self.root is None:
+            return None
+        elif self.root.left is None and self.root.right is None:
+            self.root = None
+        else:
+            if node.parent.left is node:
+                node.parent.left = None
             else:
-                break
+                node.parent.right = None
 
-    def extract_max(self):
-        if len(self.heap) > 1:
-            max_item = self.heap[0]
-            self.heap[0] = self.heap.pop()
-            self._percolate_down(0)
-            return max_item
-        elif len(self.heap) == 1:
-            return self.heap.pop()
-        else:
-            return None
+    def heapify_up(self, node: Node) -> None:
+        curr = node
+        while curr is not self.root and curr.value < curr.parent.value:
+            curr.value, curr.parent.value = curr.parent.value, curr.value
+            curr = curr.parent
 
-    def _percolate_down(self, i):
-        left = self.left(i)
-        right = self.right(i)
-        largest = i
-        if left < len(self.heap) and self.heap[left] > self.heap[largest]:
-            largest = left
-        if right < len(self.heap) and self.heap[right] > self.heap[largest]:
-            largest = right
-        if largest != i:
-            self.heap[i], self.heap[largest] = self.heap[largest], self.heap[i]
-            self._percolate_down(largest)
+    def heapify_down(self, node: Node) -> None:
+        curr = node
+        while curr.left is not None or curr.right is not None:
+            if curr.right is None or curr.left.value < curr.right.value:
+                if curr.left is not None and curr.left.value < curr.value:
+                    curr.value, curr.left.value = curr.left.value, curr.value
+                    curr = curr.left
+                else:
+                    break
+            else:
+                if curr.right is not None and curr.right.value < curr.value:
+                    curr.value, curr.right.value = curr.right.value, curr.value
+                    curr = curr.right
+                else:
+                    break
