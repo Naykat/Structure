2bc2f97fd15b0f968b631316fab5fb3129296505
Naykat <65075625+Naykat@users.noreply.github.com>
2023-03-31 22:52:56 +0300

    Global rework
    [+] Added Node structure
    [+] More functions

--- Stack.py
+++ Stack.py
@@ -1,18 +1,48 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
 class Stack:
     def __init__(self):
-        self.items = []
+        self.top = None
+        self.size = 0
 
-    def is_empty(self):
-        return self.items == []
-
-    def push(self, item):
-        self.items.append(item)
+    def push(self, data):
+        node = Node(data)
+        node.next = self.top
+        self.top = node
+        self.size += 1
 
     def pop(self):
-        return self.items.pop()
+        if self.top is None:
+            return None
+        data = self.top.data
+        self.top = self.top.next
+        self.size -= 1
+        return data
 
     def peek(self):
-        return self.items[-1]
+        if self.top is None:
+            return None
+        return self.top.data
 
-    def size(self):
-        return len(self.items)
+    def is_empty(self):
+        return self.top is None
+
+    def get_size(self):
+        return self.size
+
+    def search(self, data):
+        node = self.top
+        position = 1
+        while node is not None:
+            if node.data == data:
+                return position
+            node = node.next
+            position += 1
+        return -1
+
+    def clear(self):
+        self.top = None
+        self.size = 0
